9142df9d45564d519d97269334698393c1647204
katratzi <clifford@Richards-MacBook-Pro.local>
2020-10-21 22:13:38 +0800

    get elements by and query selector

--- dom/index.js
+++ dom/index.js
@@ -1,2 +1,27 @@
-
+// document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML = "Angela"
 document.querySelector("ul").lastElementChild.innerHTML = "Woohoo";
+
+document.getElementsByTagName("li")[2].style.color = "purple";
+document.getElementsByTagName("li").length;
+
+document.getElementsByClassName("btn").length;
+
+// single element
+document.getElementById("title").innerHTML = "goodbye"; 
+
+
+// these always get the FIRST
+// query for first h1
+document.querySelector("h1");
+// combining selectors like css
+document.querySelector("li a");
+// and more specific selector
+document.querySelector("li.item");
+// hierarchical selector
+document.querySelector("#list a");
+
+// to get all of them...use 
+document.querySelectorAll("item");
+
+// example usage
+document.querySelector("li a").style.color = "red";
