ee25b976e31b6e4938b0c45dda3c838518efd16f
katratzi <clifford@Richards-MacBook-Pro.local>
2020-10-23 18:26:59 +0800

    random dice roll function

--- dicee/dicee.js
+++ dicee/dicee.js
@@ -1,4 +1,26 @@
+function rollDice() {
+  var roll = Math.random() * 6;
+  roll = Math.floor(roll) + 1;
+  return roll;
+}
 
-var diceRoll = Math.random() * 6;
-diceRoll = Math.floor(diceRoll) + 1;
-console.log("roll is " + diceRoll);
+var diceRoll1 = rollDice();
+console.log("roll 1 is " + diceRoll1);
+
+// debug
+document.querySelector(".img1").getAttribute("src");
+// set to new image
+document
+  .querySelector(".img1")
+  .setAttribute("src", "images/dice" + diceRoll1 + ".png");
+
+// repeat for dice 2
+var diceRoll2 = rollDice();
+console.log("roll 2 is " + diceRoll2);
+
+// debug
+document.querySelector(".img2").getAttribute("src");
+// set to new image
+document
+  .querySelector(".img2")
+  .setAttribute("src", "images/dice" + diceRoll2 + ".png");
