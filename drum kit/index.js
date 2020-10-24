6e51abc557e4ae21f3980f7c5d4b510ac3b3bdba
katratzi <ric_clifford@hotmail.com>
2020-10-24 16:44:32 +0800

    play the right sound for each button

--- drum kit/index.js
+++ drum kit/index.js
@@ -13,14 +13,41 @@ var allDrumButtons = document.querySelectorAll(".drum");
 for (var i = 0; i < allDrumButtons.length; i++) {
   //   allDrumButtons[i].addEventListener("click", handleClick);
   allDrumButtons[i].addEventListener("click", function () {
-    // playing audio
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
-
-    // use the 'this' keyword to change color
-    console.log(this);
-    console.log(this.innerHTML);
-    this.style.color = "white";
+    //play sound depending on key
+    var buttonInnterHtml = this.innerHTML;
+    switch (buttonInnterHtml) {
+      case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "j":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+      case "k":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+      case "l":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+      default:
+        console.log(buttonInnterHtml);
+        break;
+    }
   });
 }
 
