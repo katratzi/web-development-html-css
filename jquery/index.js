e9edd0f6ec2a21a46ed4ab9bf24e2e00027f0583
katratzi <ric_clifford@hotmail.com>
2020-10-27 11:33:28 +0800

    adding event listeners

--- jquery/index.js
+++ jquery/index.js
@@ -18,7 +18,6 @@ $("h1").removeClass("big-title");
 
 // find out if we have a class
 console.log($("h1").hasClass("margin-50"));
-
 $("h1").text("Bye");
 
 // this affects all buttons
@@ -35,3 +34,36 @@ $("a").attr("href", "http://www.bing.com");
 
 // when we add classes to our elements, can get these with
 $("h1").attr("class");
+
+// event listeners - adding
+$("h1").click(function () {
+  console.log("I am clicked");
+});
+
+// old javascript
+// for (var i = 0; i < 5; i++) {
+//   document.querySelectorAll("button")[i].addEventListener("click", function () {
+//     document.querySelector("h1").style.color = "purple";
+//   });
+// }
+
+// can add event listeners to all of the buttons
+$("button").click(function () {
+  $("h1").css("color", "purple");
+});
+
+// looking for keypress events
+$("input").keypress(function (event) {
+  console.log(event.key);
+});
+
+// add to whole page using document - no ""
+$(document).keypress(function (event) {
+  console.log(event.key);
+  $("h1").text(event.key); // change h1 to our keypress
+});
+
+// using the 'on' method for adding events
+$("h1").on("mouseover", function (event) {
+  $("h1").css("color", "green");
+});
