459183370e34f437dc1423b8efb126cb3db88048
katratzi <ric_clifford@hotmail.com>
2020-10-28 09:04:19 +0800

    keypress to start game

--- simon game/game.js
+++ simon game/game.js
@@ -1,7 +1,22 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var gameHasStarted = false;
+var level = 0;
 
+// start up - ask for keypress
+$("#level-title").text("Press A Key to Start");
+
+// on the first keypress, start the level
+$(document).on("keypress", function () {
+  if (!gameHasStarted) {
+    nextSequence();
+    gameHasStarted = true;
+    $("#level-title").text("Level: " + level);
+  }
+});
+
+// next sequence in game
 function nextSequence() {
   // get a random color
   var randomNumber = Math.floor(Math.random() * 4);
@@ -12,18 +27,31 @@ function nextSequence() {
   $("#" + randomChosenColor)
     .animate({ opacity: 0.1 }, 100)
     .animate({ opacity: 1 }, 100);
-  // play a sound
-  var sound = new Audio("sounds/" + randomChosenColor + ".mp3");
+  playSound(randomChosenColor);
+  // update level each time
+  level++;
+  $("#level-title").text("Level: " + level);
+}
+
+// helper for playing back sounds
+function playSound(name) {
+  var sound = new Audio("sounds/" + name + ".mp3");
   sound.play();
 }
 
-// nextSequence();
+// player input - animate button
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+// player clicking on buttons
 $(".btn").on("click", function () {
-  console.log($(this).attr("id"));
   var userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
   console.log(userClickedPattern);
-  //   var sound = new Audio("sounds/" + "blue" + ".mp3");
-  //   sound.play();
-  //   nextSequence();
+  animatePress(userChosenColor);
+  playSound(userChosenColor);
 });
