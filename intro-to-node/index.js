7e49a7e4155116f16f4d6bd0a27cff976f3a6f9c
katratzi <ric_clifford@hotmail.com>
2020-10-31 12:20:30 +0800

    using a couple of node packages with npm init

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,6 +1,14 @@
 //jshint esversion:6
-const fs = require("fs");
-fs.copyFileSync("file1.txt", "file2.txt");
+// const fs = require("fs");
+// fs.copyFileSync("file1.txt", "file2.txt");
 
 // just run with node index.js
 // ctrl c twice to exit or type .exit
+
+// using a couple of npm packages
+// install simply with npm init packagename
+const superheroes = require("superheroes");
+const supervillains = require("supervillains");
+var mySuperheroName = superheroes.random();
+var mySupervillainName = supervillains.random();
+console.log(mySuperheroName + " vs " + mySupervillainName);
