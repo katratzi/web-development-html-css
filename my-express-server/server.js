51b6dcbc2146b0f3dd1f621ce41e1bb141ce676e
katratzi <ric_clifford@hotmail.com>
2020-11-05 23:01:02 +0800

    sending a simple response on our server

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+// we 'get' a message from a user requesting something from us
+// often shortened to req, res
+app.get("/", function (request, response) {
+  //   console.log(request);
+  response.send("<h1>hello world</h1>");
+});
+
 // start the server up listening on port 3000
 // quick debug message as callback so we know it's working
 app.listen(3000, function () {
