4a36f5ab133f8c5e7cb41092f3084e57a014c9f1
katratzi <ric_clifford@hotmail.com>
2020-11-05 23:09:54 +0800

    a bit about routes and nodemon :)

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -8,8 +8,29 @@ app.get("/", function (request, response) {
   response.send("<h1>hello world</h1>");
 });
 
+// contact route
+app.get("/contact", function (request, response) {
+  //   console.log(request);
+  response.send("contact me at ric@clifford.com");
+});
+
+// about route
+app.get("/about", function (request, response) {
+  response.send("I'm Richard.  I'm a <em>pro</em>grammer.  I like Star Trek");
+});
+
+// about hobbies
+app.get("/hobbies", function (request, response) {
+  response.send("Hot chicks and Star Trek");
+});
+
 // start the server up listening on port 3000
 // quick debug message as callback so we know it's working
 app.listen(3000, function () {
   console.log("server started on port 3000");
 });
+
+// so we don't have to keep restarting node
+// install npm instal -g nodemon
+// run as
+// nodemon server.js
