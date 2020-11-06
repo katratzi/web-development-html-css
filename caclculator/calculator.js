aafb5dadd9b9f947533da4d0e122e801adc4bbc4
katratzi <ric_clifford@hotmail.com>
2020-11-06 21:52:29 +0800

    extending server to create a bmi calculator

--- caclculator/calculator.js
+++ caclculator/calculator.js
@@ -11,6 +11,12 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+// use __dirname for the path where our server is
+app.get("/bmicalculator", function (req, res) {
+  console.log(__dirname);
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
 app.post("/", function (req, res) {
   console.log(req.body);
   console.log("Num1." + req.body.num1);
@@ -20,6 +26,18 @@ app.post("/", function (req, res) {
   var num2 = Number(req.body.num2);
   var result = num1 + num2;
   res.send("The result of the calculation is " + result);
+});
+
+// post from BMI calculator
+app.post("/bmicalculator", function (req, res) {
+  console.log(req.body);
+  console.log("Weight." + req.body.weight);
+  console.log("Height." + req.body.height);
+
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height);
+  var result = weight / (height * height);
+  res.send("Your BMI is " + result.toFixed(1));
 });
 
 app.listen(3000, function () {
