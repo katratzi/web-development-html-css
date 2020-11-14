231678354330e4ea51cccb50b95f2ea010c3d23b
katratzi <ric_clifford@hotmail.com>
2020-11-14 10:54:40 +0800

    successful call to api

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const https = require("https");
 const app = express();
 
 app.get("/", function (req, res) {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?appid=26c0004e30b5b9fdb93a003835d2a8b7&q=London&units=metric";
+  https.get(url, function (response) {
+    console.log(response);
+  });
   res.send("Hello world");
 });
 
