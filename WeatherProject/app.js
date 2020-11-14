fddaa9f627b380c5aaefc3b60a1ee93ffc950428
katratzi <ric_clifford@hotmail.com>
2020-11-14 11:58:04 +0800

    sending the weather response with image to our client

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -8,29 +8,26 @@ app.get("/", function (req, res) {
     "https://api.openweathermap.org/data/2.5/weather?appid=26c0004e30b5b9fdb93a003835d2a8b7&q=London&units=metric";
   https.get(url, function (response) {
     console.log(response.statusCode);
-
     response.on("data", function (data) {
-      // this data looks like byte info
-      // we can view this at https://cryptii.com/ (hex to text)
-      console.log(data);
-      // let's convert to a javascript object by parsing
+      // convert into javascript object
       const weatherData = JSON.parse(data);
-      console.log(weatherData);
-      // opposite of this is to use stringify
-      const bio = {
-        Name: "Richard",
-        FavouriteFood: "Pancakes",
-      };
-      console.log(JSON.stringify(bio));
-
       // digging into the json data
       const weatherTemperature = weatherData.main.temp;
       const weatherDescription = weatherData.weather[0].description;
-      console.log(weatherTemperature);
-      console.log(weatherDescription);
+      // grab the icon
+      const iconId = weatherData.weather[0].icon;
+      const iconUrl = "http://openweathermap.org/img/wn/" + iconId + "@2x.png";
+      console.log(iconUrl);
+      res.write("<p>The weather is currently " + weatherDescription + "</p>");
+      res.write(
+        "<h1>The temperature in London is " +
+          weatherTemperature +
+          " degrees Celsius</h1>"
+      );
+      res.write("<img src=" + iconUrl + ">");
+      res.send();
     });
   });
-  res.send("Hello world." + response.statusCode);
 });
 
 // start the server
