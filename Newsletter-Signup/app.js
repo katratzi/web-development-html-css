f5c0f70cb134c8eb21baf0dcfbcb449610fa3c37
katratzi <ric_clifford@hotmail.com>
2020-11-15 15:33:29 +0800

    setting up static files with express app.use(express.static("public"));

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -5,8 +5,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// in order to serve up static files, need to set up express with a particular folder
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-  res.send("Hello world");
+  //   res.send("Hello world");
+  res.sendFile(__dirname + "/signup.html");
 });
 
 app.listen(3000, function () {
