8ee8994da9bb77d00d8363e11f161c964ad6e8a7
katratzi <ric_clifford@hotmail.com>
2020-11-21 16:54:24 +0800

    get data from signup and post to mailchimp newsletter

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+const { json } = require("body-parser");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,8 +21,45 @@ app.post("/", function (req, res) {
   const lastName = req.body.lastName;
   const email = req.body.email;
   console.log(`Name is ${firstName} ${lastName} and ${email}`);
+
+  // create object format for our api call
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  // create json string
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us7.api.mailchimp.com/3.0/lists/c7d2e88aeb";
+  const options = {
+    method: "POST",
+    auth: "helloworld:882f4a160ac0a42a81f66445ae4cbd35-us7",
+  };
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+    console.log("hello");
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
+
+// list id
+// c7d2e88aeb
+// api key
+// 882f4a160ac0a42a81f66445ae4cbd35-us7
