bec75b91e9be1aa5930d1beefd86f46010b96616
katratzi <ric_clifford@hotmail.com>
2020-12-12 18:40:20 +0000

    render a simple view with the day passed as a value

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,22 +3,25 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 // setup body parser and a public directory
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
+
 app.get("/", function (req, res) {
     var today = new Date();
+    var day = "";
     if (today.getDay() === 6 || today.getDay() === 0) {
-        res.write("<h1>Yay! It's the weekend</h1>");
-        res.send();
+        day = "Weekend";
     }
     else {
-        // write html sequentially then send
-        res.write("<h1>It is not the weekend</h1>")
-        res.write("<p>Oh dear :( work today</p>");
-        res.send();
+        day = "Weekday";
     }
+
+    res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
