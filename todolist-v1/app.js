d6832d4100101474db450d647b3e3a9f13e54633
katratzi <ric_clifford@hotmail.com>
2020-12-14 21:53:42 +0000

    button has a name and value using this we can check where the post request came from and act accordingly

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -23,9 +23,18 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
+    console.log(req.body);
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+
+    // which list does this belong to
+    if (req.body.list === "Homework") {
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else {
+        items.push(item);
+        res.redirect("/");
+    }
 });
 
 
@@ -34,11 +43,6 @@ app.get("/work", function (req, res) {
     res.render("list", { listTitle: "Homework", newListItems: workItems });
 });
 
-app.post("/work", function (req, res) {
-    let item = req.body.newItem;
-    workItems.push(item);
-    res.redirect("/work");
-});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
