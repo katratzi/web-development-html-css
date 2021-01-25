863c1461f0445b0acf09da574b3e5759acf7b241
katratzi <ric_clifford@hotmail.com>
2021-01-25 12:16:39 +0000

    insert three fruits into the db

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -8,11 +8,46 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'fruitsDB';
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function (err, client) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
+MongoClient.connect(url,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    function (err, client) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
 
-    const db = client.db(dbName);
+        const db = client.db(dbName);
 
-    client.close();
-});
+        insertDocuments(db, function () {
+            client.close();
+        });
+    });
+
+// ran this with node app.js 
+
+const insertDocuments = function (db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    // Insert some documents
+    collection.insertMany([
+        {
+            name: "Apple",
+            score: 8,
+            review: "Great fruit"
+        },
+        {
+            name: "Orange",
+            score: 6,
+            review: "Kinda sour"
+        },
+        {
+            name: "Banana",
+            score: 9,
+            review: "Great stuff"
+        }
+    ], function (err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 fruits into the collection");
+        callback(result);
+    });
+}
