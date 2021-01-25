d1e9230e6f3adf1ca1123784a87530f568b39542
katratzi <ric_clifford@hotmail.com>
2021-01-25 12:24:40 +0000

    find objects from the db

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -16,7 +16,11 @@ MongoClient.connect(url,
 
         const db = client.db(dbName);
 
-        insertDocuments(db, function () {
+        // insertDocuments(db, function () {
+        //     client.close();
+        // });
+
+        findDocuments(db, function () {
             client.close();
         });
     });
@@ -49,5 +53,17 @@ const insertDocuments = function (db, callback) {
         assert.equal(3, result.ops.length);
         console.log("Inserted 3 fruits into the collection");
         callback(result);
+    });
+}
+
+const findDocuments = function (db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    // Find some documents
+    collection.find({}).toArray(function (err, fruits) {
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.log(fruits)
+        callback(fruits);
     });
 }
