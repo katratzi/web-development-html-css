f83884ebbeba7656dc18bd46500d05267dffb844
katratzi <ric_clifford@hotmail.com>
2021-01-26 21:23:36 +0000

    mongoose to connect/create db add a fruit into the collection

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -1,69 +1,24 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Database Name
-const dbName = 'fruitsDB';
+const fruitSchema = new mongoose.Schema({
+    name: String,
+    rating: Number,
+    review: String
+});
 
-// Use connect method to connect to the server
-MongoClient.connect(url,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    function (err, client) {
-        assert.equal(null, err);
-        console.log("Connected successfully to server");
+// first is the name of the collection, then the schema for it
+// by convention use the singular...and mongoose cleverly creates it with the multiple
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-        const db = client.db(dbName);
+// now create the object
+const fruit = new Fruit({
+    name: "Apple",
+    rating: 7,
+    review: "Pretty solid as a fruit."
+});
 
-        // insertDocuments(db, function () {
-        //     client.close();
-        // });
-
-        findDocuments(db, function () {
-            client.close();
-        });
-    });
-
-// ran this with node app.js 
-
-const insertDocuments = function (db, callback) {
-    // Get the documents collection
-    const collection = db.collection('fruits');
-    // Insert some documents
-    collection.insertMany([
-        {
-            name: "Apple",
-            score: 8,
-            review: "Great fruit"
-        },
-        {
-            name: "Orange",
-            score: 6,
-            review: "Kinda sour"
-        },
-        {
-            name: "Banana",
-            score: 9,
-            review: "Great stuff"
-        }
-    ], function (err, result) {
-        assert.equal(err, null);
-        assert.equal(3, result.result.n);
-        assert.equal(3, result.ops.length);
-        console.log("Inserted 3 fruits into the collection");
-        callback(result);
-    });
-}
-
-const findDocuments = function (db, callback) {
-    // Get the documents collection
-    const collection = db.collection('fruits');
-    // Find some documents
-    collection.find({}).toArray(function (err, fruits) {
-        assert.equal(err, null);
-        console.log("Found the following records");
-        console.log(fruits)
-        callback(fruits);
-    });
-}
+// this then creates it into the collection
+// replaces the insert documents stuff in the native driver
+fruit.save();
