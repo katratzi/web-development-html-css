9031dd3787346ad6067a741c8ae046277a74b197
katratzi <ric_clifford@hotmail.com>
2021-02-08 21:09:10 +0000

    add items to our todo list

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -53,15 +53,16 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName
+  });
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  // save to db, shortcut method instead of insert
+  item.save();
+
+  res.redirect("/");
+
 });
 
 app.get("/work", function (req, res) {
