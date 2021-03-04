b6b079798e660b1cf0baa48b1df1a21e7e35b6b4
katratzi <ric_clifford@hotmail.com>
2021-03-04 19:27:43 +0000

    lodash to format the route correctly

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 
@@ -118,7 +119,7 @@ app.post("/delete", function (req, res) {
 
 // if we get custom list name from the routed paramater
 app.get("/:customListName", function (req, res) {
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({ name: customListName }, function (err, foundList) {
     // if empty create a new list and add default items
