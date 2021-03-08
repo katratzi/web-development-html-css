9a0e417a57ada67430525f437f0bd67a274e8dd7
katratzi <ric_clifford@hotmail.com>
2021-03-08 18:15:48 +0000

    connect to our database and create schema plus model with mongoose

--- wiki-api/app.js
+++ wiki-api/app.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const mongoose = require("mongoose");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// database setup - connection
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true });
+// database schema
+const articleSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+// the model
+const Article = mongoose.model("Article", articleSchema);
+
+// home route
 app.get("/", function (req, res) {
     res.send('Hello World!');
-
 });
 
+// start up the server up
 app.listen(3000, function () {
     console.log('Server started on port 3000');
 });
