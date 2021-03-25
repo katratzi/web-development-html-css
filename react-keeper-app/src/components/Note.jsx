1a3805d937634551c2a9e5c6d62fb7c929414ec9
katratzi <ric_clifford@hotmail.com>
2021-03-25 11:06:07 +0000

    basic component setup

---
+++ react-keeper-app/src/components/Note.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Note() {
+    return <div>
+        <p>This is a note</p>
+    </div>;
+}
+
+export default Note;
--- react-keeper-app/src/index.js
+++ react-keeper-app/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./components/App";
 
 
 ReactDOM.render(
-  <h1>Hello world</h1>,
+  <App />,
   document.getElementById('root')
 );
 
 
-//1. Create a new React app.
-//2. Create a App.jsx component.
-//3. Create a Header.jsx component that renders a <header> element
-//to show the Keeper App name in an <h1>.
+//* 1. Create a new React app.
+//* 2. Create a App.jsx component.
+//* 3. Create a Header.jsx component that renders a <header> element
+//* to show the Keeper App name in an <h1>.
 //4. Create a Footer.jsx component that renders a <footer> element
 //to show a copyright message in a <p> with a dynamically updated year.
 //5. Create a Note.jsx component to show a <div> element with a
