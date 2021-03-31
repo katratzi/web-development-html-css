fb2a00533dc7f4608870dff0562dd279b532deff
katratzi <ric_clifford@hotmail.com>
2021-03-31 12:32:01 +0100

    create note controlled

--- react-keeper-app/src/components/CreateArea.jsx
+++ react-keeper-app/src/components/CreateArea.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+    // make this a controlled component
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    });
+
+    function handleChange(event) {
+
+        const newValue = event.target.value;
+        const name = event.target.name;
+
+        // use spread operator for prev value, and change the new one
+        setNote(function (prevNote) {
+            return {
+                ...prevNote,
+                [name]: newValue
+            };
+        })
+    }
+
+    // use the note.properties and use same handleChange method 
+    // using the name attribute to determine what we changed
     return (
         <div>
             <form>
-                <input name="title" placeholder="Title" />
-                <textarea name="content" placeholder="Take a note..." rows="3" />
+                <input name="title"
+                    placeholder="Title"
+                    value={note.title} onChange={handleChange} />
+                <textarea name="content"
+                    placeholder="Take a note..." rows="3"
+                    value={note.content} onChange={handleChange} />
                 <button>Add</button>
             </form>
         </div>
